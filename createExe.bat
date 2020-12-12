96e0780d9bb105f86a57f97494947148ed388519
fazalfarhan01 <fazal.farhan@gmail.com>
2020-12-12 14:07:14 +0530

    automating compilation

--- createExe.bat
+++ createExe.bat
@@ -1,1 +1,4 @@
 .\env\Scripts\python.exe -m eel .\main.py .\web --onefile --icon=.\web\images\favicon.ico -n Auto-CPY.exe
+move /Y .\dist\Auto-CPY.exe .
+del Auto-CPY.exe.spec /f /q
+"C:\Program Files (x86)\NSIS\makensis.exe" .\Auto-CPY.nsi
